091acc0847bbb2097ab43a0a41a4c8a826c091a3
Rafael Carvalho <rafael.c.carvalho@gmail.com>
2015-12-21 10:27:21 -0200

    Lazy evaluation.

--- temp.hs
+++ temp.hs
@@ -217,3 +217,19 @@ isChoice :: Eq a => [a] -> [a] -> Bool
 isChoice [] _ = True
 isChoice (x : xs) [] = False
 isChoice (x : xs) ys = elem x ys && isChoice xs (removeone x ys)
+
+ones :: [Int]
+ones = 1 : ones
+
+primes :: [Int]
+primes =  sieve [2..]
+
+sieve        :: [Int] -> [Int]
+sieve (p:xs) =  p : sieve [x | x <- xs, x `mod` p /= 0]
+
+sumwith          :: Int -> [Int] -> Int
+sumwith v []     =  v
+sumwith v (x:xs) =  (sumwith $! (v + x)) xs
+
+fibs :: [Integer]
+fibs = 0 : 1 : [x + y | (x,y) <- zip fibs (tail fibs)]
